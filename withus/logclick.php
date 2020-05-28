1e4f3a977e46ab3494c74d17863a6436497c4d50
Kousei3939 <63569282+Kousei3939@users.noreply.github.com>
2020-05-28 10:17:54 +0900

    Add files via upload

--- withus/logclick.php
+++ withus/logclick.php
@@ -8,13 +8,20 @@ if(isset($_POST['x_coord']) && isset($_POST['y_coord'])){
 	$ycoord = htmlentities($_POST['y_coord']);
 	$time = date( 'Y-m-d H:i:s');
 
-   // 接続を使用する
-   $sth = $pdo->query("INSERT INTO clicks (timestamp, page, x, y) VALUES ('$time', '$page', $xcoord, $ycoord)");
-   echo "<pre>";
-   foreach($sth as $row) {
-       print_r($row);
-   }
-   echo "</pre>";
+	 // 接続を使用する
+	 $stmt = $pdo -> prepare("INSERT INTO heatmap (time, page, xcode, ycode) VALUES (:time, :page, :x_coord, :y_coord)");
+$stmt->bindParam(':time', $time, PDO::PARAM_STR);
+$stmt->bindParam(':page', $page, PDO::PARAM_STR);
+$stmt->bindParam(':x_coord', $xcoord, PDO::PARAM_INT);
+$stmt->bindParam(':y_coord', $ycoord, PDO::PARAM_INT);
+//実行
+$stmt->execute();
+  //  $sth = $pdo->query("INSERT INTO clicks (timestamp, page, x, y) VALUES ('$time', '$page', $xcoord, $ycoord)");
+//    echo "<pre>";
+//    foreach($sth as $row) {
+//        print_r($row);
+//    }
+//    echo "</pre>";
 
    // 接続を閉じる
    $sth = null;
